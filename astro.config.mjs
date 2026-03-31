7a0c894bc46442b165c6db902fccd56d50b8ec93
Milln <merlin.albes@kabelmail.de>
2026-03-31 16:38:18 +0200

    New icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [
     tailwind(),
     react(),
-    // sitemap(),
+    icon(),
   ],
   i18n: {
     defaultLocale: 'de',
