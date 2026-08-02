d3d6952e0a6015c52356aaddd1629eb14e2231ae
Merlin Albes <merlin.albes@gmail.com>
2026-08-02 19:31:34 +0200

    feat: update author sorting logic and adjust build command for production site

--- test_md.ts
+++
@@ -1,2 +0,0 @@
-import { parseMarkdown } from './src/utils/markdown.js'; // Need tsx or compiling
-console.log(parseMarkdown('**Wolf Albes und Hafida Chabi präsentieren Les Harkis. Drame ou tragédie ?**'));
